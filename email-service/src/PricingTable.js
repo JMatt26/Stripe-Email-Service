12a47863decc1d88b89467c5a155c811f78fff65
JMatt26 <jared.matthews@mail.mcgill.ca>
2024-08-07 19:21:58 -0400

    Stripe Integrated

---
+++ email-service/src/PricingTable.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+function PricingTable() {
+  // Paste the stripe-pricing-table snippet in your React component
+  return (
+      <stripe-pricing-table
+          pricing-table-id="prctbl_1Pku5iAKAncPufFNCWjhqjCW"
+          publishable-key="pk_test_51PjofhAKAncPufFNkdDYTgr3YNeGd8ksGzz2AQ4LkSWsITGEJzYLDrBjJxmtJoQvryGKBUMUfqmXSXTpc23IPiGf003Hhv263g"
+      >
+      </stripe-pricing-table>
+  );
+}
+
+export default PricingTable;
